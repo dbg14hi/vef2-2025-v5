48e5f5211f7210f104fbca39ecf26db11e87f4c9
dbg14hi <dbg14@hi.is>
2025-04-02 19:36:52 +0000

    Greinir bætt við, hýsing set up, readme.md updated

--- src/app/(base-layout)/greinar/page.tsx
+++ src/app/(base-layout)/greinar/page.tsx
@@ -2,6 +2,7 @@ import { executeQuery } from "@/lib/datocms/executeQuery";
 import { graphql } from "@/lib/datocms/graphql";
 import { notFound } from "next/navigation";
 import Link from "next/link";
+import { revalidateTag } from "next/cache";
 
 interface Greinar {
   id: string;
@@ -24,6 +25,8 @@ const query = graphql(`
 `);
 
 export default async function GreinarPage() {
+  revalidateTag('datocms');
+
   // Explicitly casting the result from executeQuery
   const { allGreinars }: { allGreinars: Greinar[] } = await executeQuery(query) as { allGreinars: Greinar[] };
 
@@ -31,16 +34,20 @@ export default async function GreinarPage() {
     return notFound();
   }
 
+  // Function to truncate text
+  const truncateText = (text: string, maxLength: number) =>
+    text.length > maxLength ? `${text.slice(0, maxLength)}...` : text;
+
   return (
     <div className="container">
-      <h3>Greinar</h3>
+      <h3 className="headName">Greinar</h3>
       <ul className="greinar-list">
         {allGreinars.map((greinar) => (
           <li key={greinar.id}>
             <Link href={`/greinar/${greinar.slug}`}>
               <h4>{greinar.title}</h4>
             </Link>
-            <p>{greinar.text}</p>
+            <p>{truncateText(greinar.text, 200)}</p>
             <small>{new Date(greinar.date).toLocaleDateString()}</small>
           </li>
         ))}
